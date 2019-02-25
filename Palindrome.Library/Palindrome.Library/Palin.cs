c01cf839c6df1f1458775e19fd05670f3db79ade
William <billycraig2009@hotmail.com>
2019-02-25 11:28:07 -0600

    palindrome checker

---
+++ Palindrome.Library/Palindrome.Library/Palin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome.Library
+{
+    public class Palin
+    {
+        private string _first;
+        private string _second;
+
+        public string First
+        { get => _first;
+            set
+            {
+                _first = value;
+            }
+        }
+        public string Second
+        { get => _second;
+          set
+            {
+                _second = value;
+            }
+        }
+
+        public string Clean(string value)
+        {
+            value.ToLower();
+            value.Replace(" ", string.Empty);
+            value.Replace(",", string.Empty);
+            value.Replace(".", string.Empty);
+            value.Replace("-", string.Empty);
+            value.Replace("?", string.Empty);
+            value.Replace(";", string.Empty);
+            value.Replace(":", string.Empty);
+            return value;
+        }
+
+        public bool Drome()
+        {
+            Clean(_first);
+            Clean(_second);
+
+            _first.Reverse();
+            return _first == _second;
+        }
+    }
+}
---
+++ Palindrome.Library/PalindromeTest.Test/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using Palindrome.Library;
+
+namespace PalindromeTest.Test
+{
+    public class UnitTest1
+    {
+        readonly Palin palindrome = new Palin();
+
+        [Fact]
+        public void Return_True_for_First_Allocation()
+        {
+            const string tFirst = "william";
+            palindrome.First = tFirst;
+            bool result = palindrome.First == tFirst;
+            Assert.True(result, "I should be True");
+
+        }
+
+        [Fact]
+        public void Return_True_for_Second_Allocation()
+        {
+            const string tSecond = "mailliw";
+            palindrome.Second = tSecond;
+            bool result = palindrome.Second == tSecond;
+            Assert.True(result, "I should be True");
+        }
+
+        [Fact]
+        public void Return_True_for_Palindrome()
+        {
+            Assert.True(palindrome.Drome(), "I should be True");
+        }
+
+        [Fact]
+        public void Return_False_for_Comparison()
+        {
+            palindrome.Second = "mail liw.";
+            string temp = palindrome.Clean(palindrome.Second);
+
+            bool result = temp == palindrome.Second;
+            Assert.True(result, "I should be False");
+        }
+    }
+
+}
