06073d7649c867c00ef50af58f56c5eb651e9046
Oswaldo Esqueda <waldo@Oswaldos-MacBook-Pro.local>
2019-02-25 11:32:03 -0600

    added palindrome test

---
+++ Assessment.Testing/PalindromeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Assessment.Library;
+using Xunit;
+
+namespace Assessment.Testing
+{
+    public class PalindromeTest
+    {
+        public void Input_NotEmpty()
+        {
+            Assert.ThrowsAny<ArgumentException>(() => Palindrome.palindrome = string.Empty);
+        }
+    }
+}
