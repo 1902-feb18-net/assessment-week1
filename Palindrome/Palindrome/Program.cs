9a985e665f5f6112f98c4400328a505362345afe
Your Name <you@example.com>
2019-02-25 11:00:08 -0600

    Got the Palindrome method working

--- Palindrome/Palindrome/Program.cs
+++ Palindrome/Palindrome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PalindromeLibrary;
 
 namespace Palindrome
 {
@@ -9,7 +10,22 @@ namespace Palindrome
             string input1 = "nurses run";
             string input2 = "racecaR";
             string input3 = "1221";
-            string input4 = "";
+            string input4 = "never odd, or even";
+            string input5 = "one two one";
+            string input6 = "123abccba123";
+
+            var result1 = PalindromeLibrary.Palindrome.CheckPalindrome(input1);
+            Console.WriteLine(result1);
+            var result2 = PalindromeLibrary.Palindrome.CheckPalindrome(input2);
+            Console.WriteLine(result2);
+            var result3 = PalindromeLibrary.Palindrome.CheckPalindrome(input3);
+            Console.WriteLine(result3);
+            var result4 = PalindromeLibrary.Palindrome.CheckPalindrome(input4);
+            Console.WriteLine(result4);
+            var result5 = PalindromeLibrary.Palindrome.CheckPalindrome(input5);
+            Console.WriteLine(result5);
+            var result6 = PalindromeLibrary.Palindrome.CheckPalindrome(input6);
+            Console.WriteLine(result6);
 
 
 
--- Palindrome/PalindromeLibrary/Palindrome.cs
+++ Palindrome/PalindromeLibrary/Palindrome.cs
@@ -2,11 +2,26 @@
 
 namespace PalindromeLibrary
 {
-    public class Palindrome
+    public static class Palindrome
     {
         public static bool CheckPalindrome(string input)
         {
+            // Get rid of whitespace
+            // http://net-informations.com/q/faq/remove.html
+            string output = input.Replace(" ", String.Empty);
+            output = output.Replace(",", String.Empty);
+            output = output.Replace(".", String.Empty);
 
+
+            // Now try and reverse the string
+            // https://www.tutorialspoint.com/Chash-program-to-check-if-a-string-is-palindrome-or-not
+            char[] ch = output.ToCharArray();
+
+            Array.Reverse(ch);
+            string reversed = new string(ch);
+
+
+            return output.Equals(reversed, StringComparison.OrdinalIgnoreCase);
 
         }
     }
