5247b3073b9c35bd2d4f65c6ed49f55be9653e09
Jeff <jeff.echeverria@gmail.com>
2019-02-25 11:11:27 -0600

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace assessment_week1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string initialString;
+            Console.Write("Enter a string to be checked if it is a palindrome:");
+            
+            initialString = Console.ReadLine();   
+            char[] stringArray = initialString.ToCharArray();
+            
+            string test = String.Empty;
+
+            for(int i = stringArray.Length -1; i > -1; i--)
+            {
+                test += stringArray[i];
+            }
+            // Console.WriteLine(test);
+
+            if (test == initialString)
+                {
+                    Console.WriteLine($"{initialString} is a palindrome");
+                }
+            else
+            {
+                Console.WriteLine($"{initialString} is not a palindrome");               
+            }
+            
+           
+
         }
     }
 }
