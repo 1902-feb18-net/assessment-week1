f62e763746732c3183a8120c3a1790158bd8dc9d
Conrad <conrad.trohajr@gmail.com>
2019-02-25 11:50:44 -0500

    Library and test packages created. CheckIfPalindrome method created.

---
+++ Palindrome/Palindrome.Library/IsPalindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Palindrome.Library
+{
+    public class IsPalindrome
+    {
+        public bool CheckIfPalindrome(string s)
+        {
+            string check = "";
+
+            if(s.Equals(null))
+            {
+                throw new ArgumentNullException("String cannot be null.", nameof(s);
+            }
+
+            foreach (char c in s)
+            {
+                check = s + check;
+            }
+
+            if (s.Equals(check)) return true;
+
+            return false;
+        }
+    }
+}
---
+++ Palindrome/Palindrome.Test/TestPalindrome.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Palindrome.Library;
+
+namespace Palindrome.Test
+{
+    public class TestPalindrome
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
